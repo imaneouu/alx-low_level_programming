2f5989cee4f01e953de405c1d3485f577a54bcbc
imaneouu <imaneouublid@gmail.com>
2023-03-16 00:56:17 -0700

    4puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
-*main - prints a text
-*Return: Always 0 (success)
-*/
+ * main - prints a text
+ * Return: Always 0 (success)
+ */
 
-int main void 
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
