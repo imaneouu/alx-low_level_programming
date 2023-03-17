a59db6bcad9d4cd47c496603dbd23be1ec6b8336
imaneouu <imaneouublid@gmail.com>
2023-03-17 05:20:42 -0700

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print small letter alphabet in reverse
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char letter = 'z';
+
+	while (letter >= 'a')
+	{
+		putchar(letter);
+		letter--;
+	}
+	putchar('\n');
+
+	return (0);
+}
