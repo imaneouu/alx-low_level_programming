791af067d1dcef2cdba538e57cae43ba728680c0
imaneouu <imaneouublid@gmail.com>
2023-04-28 03:09:33 -0700

    list

--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -1,5 +1,13 @@
-#ifndef _LIST_
-#define _LIST_
+#ifndef LISTS_H
+#define LISTS_H
+
+/*
+* Desc: header file containinf prototypes and definitions
+*	for all functions and types written in this proj
+*/
+
+#include <stdlib.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -7,7 +15,6 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
  */
 typedef struct list_s
 {
@@ -15,12 +22,11 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
-int _putchar(char c);
+
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-int _strlen(const char *s);
-void before_main(void)__attribute__((constructor));
+
 #endif
